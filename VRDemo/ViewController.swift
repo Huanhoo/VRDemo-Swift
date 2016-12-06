0b2d27ab384569a885bcf55f6da6a09d34f7e6de
huhuan <huan.hu@yinyuetai.com>
2016-12-06 11:33:53 +0800

    Initial Commit

---
+++ VRDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VRDemo
+//
+//  Created by huhuan on 2016/12/6.
+//  Copyright © 2016年 Huanhoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
